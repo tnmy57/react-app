d647500460be1df2302cf2d4c8ed95631b04d60f
Tanmoy <tnmy57@gmail.com>
2023-08-11 09:05:44 +0530

    Cleaned previous app for Assignment 3

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import "./video.css"
+import "./video.css";
 import Border from "./border";
 
-const Video = ({ id, title, channel= "Coder Dost", views, time }) => {
-let url = `https://picsum.photos/id/${id}/200/100`;
+const Video = ({ id, title, channel = "Coder Dost", views, time }) => {
+  let url = `https://picsum.photos/id/${id}/200/100`;
   return (
     <div className="container">
       <div className="pic">
         <img src={url} alt="" />
       </div>
-      
+
       <div className="title">{title}</div>
       <div className="channel">{channel}</div>
-      <Border><div className="views">
-        {views} <span>•</span> {time}
-      </div></Border>
+      <Border>
+        <div className="views">
+          {views} <span>•</span> {time}
+        </div>
+      </Border>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,28 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import App2 from "./App2";
-const stem = ReactDOM.createRoot(document.getElementById("stem"));
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// value
-  // ? root.render(
-  //     <React.StrictMode>
-  //       <App />
-  //     </React.StrictMode>
-  //   )
-  // : stem.render(
-  //     <React.StrictMode>
-  //       <App2 />
-  //     </React.StrictMode>
-  //   );
 
-     root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    )
-    stem.render(
-      <React.StrictMode>
-        <App2 />
-      </React.StrictMode>
-    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
