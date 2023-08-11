e2c49111d1ce74036bd2619d3f48a33a8b083cdf
Tanmoy <tnmy57@gmail.com>
2023-08-11 15:00:30 +0530

    assignment 3 is compleated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import "./App.css";
 
-
 function App() {
- 
+
+  function zoomin() {
+    document.getElementById("image").style.transform = "scale(1.5)";
+  }
+  function zoomout() {
+    document.getElementById("image").style.transform = "scale(1)";
+  }
+
   return (
     <div className="App">
-      
+      <img
+        id="image"
+        onMouseOver={() => {
+          zoomin();
+        }}
+        onMouseOut={() => {
+          zoomout();
+        }}
+        src="https://picsum.photos/id/233/500/250"
+        alt="rail"
+      />
+      <form>
+        <input
+          onFocus={() => {
+            console.log("Focus on the textbox");
+          }}
+          onBlur={() => {
+            console.log("Out of the textbox");
+          }}
+        ></input>
+      </form>
+      <button
+        onClick={() => {
+          console.log("hello");
+        }}
+        onDoubleClick={() => {
+          alert("hi");
+        }}
+      >
+        submit
+      </button>
     </div>
   );
 }
